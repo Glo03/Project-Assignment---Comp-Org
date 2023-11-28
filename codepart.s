c9e8a7071ca91af68f53a74de1e1273047d1abcd
Glo03 <110479000+Glo03@users.noreply.github.com>
2023-11-28 14:13:48 -0500

    starting main (line 6-10)

--- codepart.s
+++ codepart.s
@@ -3,3 +3,8 @@ prompt: .asciiz "Enter a positive integer greater than or equal to 25: "
 error_msg:  .asciiz "Invalid number. Please enter a valid positive number. \n"
 fib_msg:    .asciiz "Fibonacci sequence: "
 newline:    .asciiz "\n"
+
+  .text
+  .globl main
+
+main:
