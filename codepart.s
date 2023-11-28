f22ba83421fd3755addd238be2eb2a5137afb2d2
Glo03 <110479000+Glo03@users.noreply.github.com>
2023-11-28 14:23:55 -0500

    still working on main

--- codepart.s
+++ codepart.s
@@ -8,3 +8,13 @@ newline:    .asciiz "\n"
   .globl main
 
 main:
+  li $v0, 4       #system call (syscall) for print_str
+  la $a0, prompt  #load the address of prompt
+  syscall
+  
+  #read user input
+  li $v0, 5   #syscall (System call): read_int
+  syscall
+  move $t0, $v0   #save user input in $t0
+
+  #Verify if the entered number is valid
